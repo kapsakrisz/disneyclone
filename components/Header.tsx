c5cfa5a41271f03a941ff11e13b2869f9ba5d971
krisz <devkapsakrisz@gmail.com>
2024-01-31 11:38:11 +0000

    adding form search

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import Link from "next/link"
 import { ThemeToggler } from "./ThemeToggler"
+import SearchInput from "./Searchinput"
 
 
 
 function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+      <Link href="/" className="mr-10">
         <Image src="https://links.papareact.com/a943ae" alt="disney logo"
         width={120}
         height={100}
@@ -17,6 +18,7 @@ function Header() {
 
       </Link>
       <div className="flex space-x-2">
+        <SearchInput/>
         <ThemeToggler/>
 
       </div>
